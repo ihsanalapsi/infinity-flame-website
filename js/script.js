0e06937c4c588a7399dd864e0982ac0b6f90e364
ihsanalapsi <ihsanalapsi@gmail.com>
2023-08-19 17:10:50 +0300

    Refresh Contact Us button

---
+++ js/script.js
@@ -0,0 +1,21 @@
+$(function() {
+
+    $(document).scroll( function(){
+        var $nav = $('.nav');
+        $nav.toggleClass('scrolled' , $(this).scrollTop() > $nav.height());
+    });
+});
+var phoneNumber = "+905511681241";
+var encodedPhoneNumber = encodeURIComponent(phoneNumber);
+
+var whatsappLink = "https://api.whatsapp.com/send?phone=" + encodedPhoneNumber;
+
+// Function to open WhatsApp link
+function openWhatsAppLink() {
+    var linkElement = document.getElementById("whatsappLink");
+    linkElement.href = whatsappLink;
+    linkElement.click();
+}
+// script.js (continued)
+var buttonElement = document.querySelector(".cta button");
+buttonElement.addEventListener("click", openWhatsAppLink);
