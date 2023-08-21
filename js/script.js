d48438846e2e72e4f42f73655efe8a76a7cac4a0
ihsanalapsi <ihsanalapsi@gmail.com>
2023-08-21 19:49:59 +0300

    Added about us & our team sections

--- js/script.js
+++ js/script.js
@@ -19,3 +19,21 @@ function openWhatsAppLink() {
 // script.js (continued)
 var buttonElement = document.querySelector(".cta button");
 buttonElement.addEventListener("click", openWhatsAppLink);
+
+
+
+// Smooth scrolling for navigation links
+$(document).ready(function () {
+    $(".menu-items a").on("click", function (e) {
+        e.preventDefault();
+        var target = $(this.hash);
+        $("html, body").animate(
+            {
+                scrollTop: target.offset().top - $(".nav").outerHeight(),
+            },
+            100 // Adjust the duration here (in milliseconds)
+        );
+    });
+});
+
+
